49f5a66ad790523a314b4241bb0258bd5e21a313
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2024-05-02 20:26:58 +0400

    Tests updates

--- src/js-homework/homework5/main.js
+++ src/js-homework/homework5/main.js
@@ -1,18 +1,8 @@
-let root = document.querySelector('.root');
-function makeList(a, b){
-  let newList = [];
-  for (let i = a; i<=b; i++){
-    let el = (i % 15 === 0) ? 'FizzBuzz' : 
-                      (i % 3 === 0) ? 'Fizz' :
-                      (i % 5 === 0) ? 'Buzz' : String(i);
-    newList.push(el);
-  }
-  console.log(newList);
+import { makeList } from "./util";
 
-  // let elem = document.createElement('p');
-  // elem.innerHTML = newList.map(item => String(item))
-  // root.append(elem);
-  return newList;
-}
-// makeList(1, 100);
-export default makeList;
+const newList = makeList(1, 100);
+
+let root = document.querySelector(".root");
+let elem = document.createElement("p");
+elem.innerHTML = newList.map((item) => String(item));
+root.append(elem);
