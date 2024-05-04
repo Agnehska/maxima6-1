1e0f0b4f62f39edfd34205672704dc53fd3a60ab
Agnehska <45421349+Agnehska@users.noreply.github.com>
2024-05-04 10:45:40 +0300

    adding contacts+footer

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ import Estimate from './components/Estimate/Estimate';
 import Review from './components/Review/Review';
 import Director from './components/Director/Director';
 import Questions from './components/Questions/Questions';
+import Contacts from './components/Contacts/Contacts';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -28,6 +30,8 @@ function App() {
       <Director />
       <Questions />
       <Estimate />
+      <Contacts />
+      <Footer />
     </div>
   );
 }
