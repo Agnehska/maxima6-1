0d8b305f7db7fb2640d96379723e61879c1782d1
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2024-05-13 20:34:26 +0400

    Добавили handleChange сразу для всех полей

--- src/components/Users/UserCard/UserCard.jsx
+++ src/components/Users/UserCard/UserCard.jsx
@@ -3,77 +3,88 @@ import './UserCard.scss';
 import { deleteUsers, changeUser } from '../../../redux/slices/usersSlice';
 import { useDispatch } from 'react-redux';
 
-const UserCard = ({status, id, name, email, phone, login, type}) => {
+const UserCard = ({ status, id, name, email, phone, login, type }) => {
   const [isChanging, setIsChanging] = useState(false);
   const [userData, setUserData] = useState({ id, name, email, phone, login, type })
   const dispatch = useDispatch();
 
-  function changeItem(){
+  function changeItem() {
     dispatch(changeUser(userData));
     setIsChanging(false)
   }
 
-  function createChanges(){
+  function createChanges() {
     setIsChanging(true)
-    console.log(true)
   }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setUserData({ ...userData, [name]: value });
+  };
 
   return (
     <div className="users__card">
       <p className={`${status === 'title' ? 'users__table-name' : 'users__name'} card__elem`}>{id}</p>
-      {isChanging ? 
+      {isChanging ?
         <div className="card__elem">
-          <input type="text" className='users__input' value={userData.name} onChange={event => setUserData({...userData, name: event.target.value})}/> 
-        </div>  
-      :
+          <input
+            type="text"
+            className='users__input'
+            value={userData.name}
+            name="name"
+            onChange={event => handleChange(event)}
+          />
+        </div>
+        :
         <h4 className={`${status === 'title' ? 'users__table-name' : 'users__name'} card__elem`}>{name}</h4>
-      } 
-      {isChanging ? 
+      }
+      {isChanging ?
         <div className="card__elem">
-          <input type="text" className='users__input' value={userData.email} onChange={event => setUserData({...userData, email:event.target.value})}/> 
-        </div>  
-      :
+          <input type="text" className='users__input' value={userData.email} name="email" onChange={event => handleChange(event)} />
+        </div>
+        :
         <p className={`${status === 'title' ? 'users__table-name' : 'users__name'} card__elem`}>{email}</p>
-      } 
-      {isChanging ? 
+      }
+      {isChanging ?
         <div className="card__elem">
-          <input type="text" className='users__input' value={userData.phone} onChange={event => setUserData({...userData, phone: event.target.value})}/> 
-        </div>  
-      :
+          <input type="text" className='users__input' value={userData.phone} name="phone" onChange={event => handleChange(event)} />
+        </div>
+        :
         <p className={`${status === 'title' ? 'users__table-name' : 'users__name'} card__elem`}>{phone}</p>
       }
-      {isChanging ? 
+      {isChanging ?
         <div className="card__elem">
-          <input type="text" className='users__input' value={userData.login} onChange={event => setUserData({...userData, login: event.target.value})}/> 
-        </div>  
-      :
+          <input type="text" className='users__input' value={userData.login} name="login" onChange={event => handleChange(event)} />
+        </div>
+        :
         <p className={`${status === 'title' ? 'users__table-name' : 'users__name'} card__elem`}>{login}</p>
       }
-      {isChanging ? 
+      {isChanging ?
         <div className="card__elem">
-          <input type="text" className='users__input' value={userData.type} onChange={event => setUserData({...userData, type: event.target.value})}/> 
-        </div>  
-      :
+          <input type="text" className='users__input' value={userData.type} name="type" onChange={event => handleChange(event)} />
+        </div>
+        :
         <p className={`${status === 'title' ? 'users__table-name' : 'users__name'} card__elem`}>{type}</p>
       }
-      { status === 'title' ? 
-      <>
-        <p className="users__table-name card__elem">You can</p>
-        <p className="users__table-name card__elem">You can</p>
-      </> : 
-      <>
-        <div className="users__block card__elem">
-        {isChanging ? 
-          <button className="users__btn" onClick={changeItem}>Save</button> 
-        :
-          <button className="users__btn" onClick={createChanges}>Change</button>
-        }
-        </div>
-        <div className="users__block card__elem">
-          <button className="users__btn" onClick={() => dispatch(deleteUsers(id))}>Delete</button>
-        </div>
-      </>}
-      
+      {status === 'title' ?
+        <>
+          <p className="users__table-name card__elem">You can</p>
+          <p className="users__table-name card__elem">You can</p>
+        </> :
+        <>
+          <div className="users__block card__elem">
+            {isChanging ?
+              <button className="users__btn" onClick={changeItem}>Save</button>
+              :
+              <button className="users__btn" onClick={createChanges}>Change</button>
+            }
+          </div>
+          <div className="users__block card__elem">
+            <button className="users__btn" onClick={() => dispatch(deleteUsers(id))}>Delete</button>
+          </div>
+        </>}
+
     </div>
   );
 }
